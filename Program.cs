062a3e3beab8eef35467d1177f95d1beac8ad9c7
Ayfri <pierre.ayfri@gmail.com>
2022-10-10 09:01:56 +0200

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Games
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			Application.SetHighDpiMode(HighDpiMode.SystemAware);
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
