d45e6678668d47d8bd002e9b041cb4c5c402e627
Ayfri <pierre.ayfri@gmail.com>
2022-10-28 19:25:11 +0200

    fix: Add missing file.

---
+++ Breakout/Entities/CollisionPayload.cs
@@ -0,0 +1,34 @@
+﻿namespace Breakout;
+
+using System.Drawing;
+using Breakout.Entities;
+
+/// <summary>
+///     A class for storing data when a collision occurs.
+/// </summary>
+public sealed class CollisionPayload {
+	/// <summary>
+	///     The ball that collided with the entity.
+	/// </summary>
+	public Ball Ball;
+
+	/// <summary>
+	///     The brick that was hit.
+	/// </summary>
+	public Brick Brick;
+
+	/// <summary>
+	///     The brick position in the grid.
+	/// </summary>
+	public Point BrickPosition;
+
+	/// <summary>
+	///     The game.
+	/// </summary>
+	public GameScene Game = null!;
+
+	/// <summary>
+	///     The side of the brick that was hit.
+	/// </summary>
+	public Side Side;
+}
